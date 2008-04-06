af916b569e1e3e9eb6c0c3005ec9af3e50492f41
segphault <>
2008-04-06 07:04:58 +0000

    Added proper reply support for Jaiku and Pownce.

--- gwibber/jaiku.py
+++ gwibber/jaiku.py
@@ -8,9 +8,11 @@ SegPhault (Ryan Paul) - 01/05/2008
 """
 
 import urllib2, urllib, base64, simplejson
-import time, datetime, config, gtk, gwui
+import time, datetime, config, gtk, gwui, re
 
 from gwui import StatusMessage
+
+NONCE_PARSE = re.compile('.*_nonce" value="([^"]+)".*', re.M | re.S)
 
 def parse_time(t):
   return datetime.datetime.strptime(t, "%Y-%m-%dT%H:%M:%S %Z")
@@ -20,11 +22,13 @@ class Message:
     self.client = client
     self.account = client.account
     self.data = data
+    if data.has_key("id"): self.id = data["id"]
     self.sender = "%s %s" % (data["user"]["first_name"], data["user"]["last_name"])
     self.sender_nick = data["user"]["nick"]
     self.sender_id = data["user"]["nick"]
     self.time = parse_time(data["created_at"])
-    self.text = data["title"].replace('"', "&quot;").replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;")
+    if data.has_key("title"):
+      self.text = data["title"].replace('"', "&quot;").replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;")
     self.image = data["user"]["avatar"]
     self.bgcolor = "message_color"
     self.url = data["url"]
@@ -47,6 +51,7 @@ class Client:
 
   def can_send(self): return True
   def can_receive(self): return True
+  def can_reply(self): return True
 
   def send_enabled(self):
     return self.account["send_enabled"] and \
@@ -64,10 +69,39 @@ class Client:
         urllib.urlencode({"user": self.account["username"],
           "personal_key":self.account["password"]}))).read())
 
+  def get_reply_data(self, msg):
+    return simplejson.loads(urllib2.urlopen(urllib2.Request(
+      "http://%s.jaiku.com/presence/%s/json" % (msg.sender_nick, msg.id),
+        urllib.urlencode({"user": self.account["username"],
+          "personal_key":self.account["password"]}))).read())
+
+  def get_replies(self, msg):
+    yield msg
+    for data in self.get_reply_data(msg)["comments"]:
+      yield Comment(self, data)
+
   def get_messages(self):
     for data in self.get_data()["stream"]:
       if data.has_key("id"): yield Message(self, data)
       else: yield Comment(self, data)
+
+  def get_nonce(self, msg):
+    try:
+      page = urllib2.urlopen(urllib2.Request(
+        "http://%s.jaiku.com/presence/%s" % (msg.sender_nick, msg.id),
+          urllib.urlencode({"user": self.account["username"], 
+            "personal_key":self.account["password"]}))).read()
+      
+      return NONCE_PARSE.match(page, 1).group(1)
+    except: return None
+
+  def transmit_reply(self, msg, message):
+    nonce = self.get_nonce(msg)
+    if nonce:
+      return urllib2.urlopen(urllib2.Request(
+        "http://%s.jaiku.com/presence/%s" % (msg.sender_nick, msg.id),
+          urllib.urlencode({"user": self.account["username"], "_nonce": nonce, 
+            "personal_key":self.account["password"], "comment": message}))).read()
 
   def transmit_status(self, message):
     return urllib2.urlopen(urllib2.Request(
--- gwibber/pownce.py
+++ gwibber/pownce.py
@@ -15,7 +15,7 @@ from gwui import StatusMessage, ConfigPanel
 API_KEY = "w5t07ju7t1072o1wfx8l9012a51fdabq"
 
 def parse_time(t):
-  return datetime.datetime.fromtimestamp(t)
+  return datetime.datetime.utcfromtimestamp(t)
 
 class Message:
   def __init__(self, client, data):
@@ -29,12 +29,19 @@ class Message:
     self.text = data["body"]
     self.image = data["sender"]["profile_photo_urls"]["medium_photo_url"]
     self.bgcolor = "message_color"
-    self.url = data["permalink"]
+    if data.has_key("permalink"):
+      self.url = data["permalink"]
     self.profile_url = data["sender"]["permalink"]
+    self.id = data["id"]
 
   def is_new(self):
     return self.time > datetime.datetime(
       *time.strptime(config.Preferences()["last_update"])[0:6])
+
+class Comment(Message):
+  def __init__(self, client, data):
+    Message.__init__(self, client, data)
+    self.bgcolor = "comment_color"
 
 class Client:
   def __init__(self, acct):
@@ -42,6 +49,7 @@ class Client:
 
   def can_send(self): return True
   def can_receive(self): return True
+  def can_reply(self): return True
 
   def send_enabled(self):
     return self.account["send_enabled"] and \
@@ -61,14 +69,78 @@ class Client:
     return urllib2.urlopen(urllib2.Request(
       url, data, {"Authorization": self.get_auth()})).read()
 
+  def get_reply_data(self, msg):
+    return simplejson.loads(self.connect(
+      "http://api.pownce.com/2.0/notes/%s.json?app_key=%s&show_replies=true" % (msg.id, API_KEY)))
+
+  def get_replies(self, msg):
+    yield msg
+    messages = self.get_reply_data(msg)
+    if messages.has_key("replies"):
+      for data in messages["replies"]:
+        yield Comment(self, data)
+
+  def transmit_reply(self, msg, message):
+    return self.connect("http://api.pownce.com/2.0/send/reply.json",
+        urllib.urlencode({"note_body": message, "app_key": API_KEY, "reply_to": msg.id}))
+    
   def get_data(self):
     return simplejson.loads(self.connect(
       "http://api.pownce.com/2.0/note_lists/%s.json?app_key=%s" % (self.account["username"], API_KEY)))
 
   def get_messages(self):
     for data in self.get_data()["notes"]:
-      yield Message(self, data)
+      if data["type"] == "message": yield Message(self, data)
+      else: yield Comment(self, data)
 
   def transmit_status(self, message):
     return self.connect("http://api.pownce.com/2.0/send/message.json",
         urllib.urlencode({"note_body":message, "app_key": API_KEY, "note_to": "public"}))
+
+  def transmit_link(self, message):
+    return self.connect("http://api.pownce.com/2.0/send/message.json",
+        urllib.urlencode({"note_body":message, "app_key": API_KEY, "note_to": "public"}))
+
+class ConfigPanel(gwui.ConfigPanel):
+  def ui_appearance(self):
+    f = gwui.ConfigFrame("Appearance")
+    t = gtk.Table()
+    t.attach(gtk.Label("Message color:"), 0, 1, 0, 1, gtk.SHRINK)
+    t.attach(self.account.bind(gtk.ColorButton(), "message_color"), 1, 2, 0, 1)
+    t.attach(gtk.Label("Comment color:"), 0, 1, 1, 2, gtk.SHRINK)
+    t.attach(self.account.bind(gtk.ColorButton(), "comment_color"), 1, 2, 1, 2)
+    f.add(t)
+    return f
+
+if __name__ == "__main__":
+  import gnome.ui, time
+
+
+  window = gtk.Window()
+  window.connect("destroy", gtk.main_quit)
+  window.set_title("Post an Event")
+  window.set_border_width(10)
+
+  event_name = gtk.Entry()
+  event_location = gtk.Entry()
+  calendar = gnome.ui.DateEdit(time.time(), True, False)
+
+  t = gtk.Table()
+  t.set_col_spacings(5)
+  t.set_row_spacings(5)
+
+  t.attach(gtk.Label("Event name:"), 0, 1, 0, 1, gtk.SHRINK)
+  t.attach(event_name, 1, 2, 0, 1)
+  t.attach(gtk.Label("Event location:"), 0, 1, 1, 2, gtk.SHRINK)
+  t.attach(event_location, 1, 2, 1, 2)
+  t.attach(gtk.Label("Event Date:"), 0, 1, 2, 3, gtk.SHRINK)
+  t.attach(calendar, 1, 2, 2, 3)
+
+  vbox = gtk.VBox(spacing=10)
+  vbox.pack_start(t, False, False)
+  vbox.pack_start(gtk.TextView())
+
+  window.add(vbox)
+  window.show_all()
+
+  gtk.main()
