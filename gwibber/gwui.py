315ff7ab1a372f7bcf7c807105a55e0dd4bf7ea5
Dominic Evans <oldmanuk@gmail.com>
2008-12-20 14:25:09 -0800

    Automatically resize large profile images

--- gwibber/gwui.py
+++ gwibber/gwui.py
@@ -8,6 +8,7 @@ SegPhault (Ryan Paul) - 05/26/2007
 
 import webkit, gintegration, microblog, gtk, resources
 import urllib2, hashlib, time, os, simplejson
+import Image
 
 DEFAULT_UPDATE_INTERVAL = 1000 * 60 * 5
 IMG_CACHE_DIR = "%s/.gwibber/imgcache" % os.path.expanduser("~")
@@ -73,11 +74,20 @@ def image_cache(url, cache_dir = IMG_CACHE_DIR):
   if not os.path.exists(cache_dir): os.makedirs(cache_dir)
   encoded_url = hashlib.sha1(url).hexdigest()
   if len(encoded_url) > 200: encoded_url = encoded_url[::-1][:200]
-  img_path = os.path.join(cache_dir, encoded_url).replace("\n","")
+  img_path = os.path.join(cache_dir, encoded_url + '.jpg').replace("\n","")
 
   if not os.path.exists(img_path):
     output = open(img_path, "w+")
     output.write(urllib2.urlopen(url).read())
     output.close()
+    try:
+        image = Image.open(img_path)
+        (x, y) = image.size
+        if x != 48 or y != 48:
+            image = image.resize((48, 48), Image.ANTIALIAS)
+            image.save(img_path)
+    except Exception:
+        from traceback import format_exc
+        print format_exc()
 
   return img_path
