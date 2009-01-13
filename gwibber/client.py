196490831442504b1cf3e31ee2d839adfb499232
Dominic Evans <oldmanuk@gmail.com>
2009-01-13 09:57:07 +0000

    Make the 'append colon after username' when replying behaviour optional via a preference (currently under experimental with url shortening in preference pane until appropriate layout is determined)

--- gwibber/client.py
+++ gwibber/client.py
@@ -404,7 +404,7 @@ class GwibberClient(gtk.Window):
 
     if acct.supports(microblog.can.REPLY):
       self.input.grab_focus()
-      self.input.set_text("@%s: " % message.sender_nick)
+      self.input.set_text("@%s%s" % (message.sender_nick, self.preferences['reply_append_colon'] and ': ' or ' '))
       self.input.set_position(-1)
 
       self.message_target = message
@@ -698,7 +698,7 @@ class GwibberClient(gtk.Window):
     dialog = glade.get_widget("pref_dialog")
     dialog.show_all()
 
-    for widget in ["show_notifications", "refresh_interval", "minimize_to_tray", "hide_taskbar_entry", "shorten_urls"]:
+    for widget in ["show_notifications", "refresh_interval", "minimize_to_tray", "hide_taskbar_entry", "shorten_urls", "reply_append_colon"]:
       self.preferences.bind(glade.get_widget("pref_%s" % widget), widget)
 
     self.preferences.bind(glade.get_widget("show_tray_icon"), "show_tray_icon")
